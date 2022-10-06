d9206391f11daadc77bc4a263874b2aa2d4c8636
Riccardo Gulin <riccardo@nucleode.com>
2022-10-06 20:37:21 +0200

    Adds Spinners & Error handling

--- src/components/ReservationsList.jsx
+++ src/components/ReservationsList.jsx
@@ -1,9 +1,12 @@
 import { Component } from "react"
-import { ListGroup } from "react-bootstrap"
+import { ListGroup, Alert } from "react-bootstrap"
+import { SpinnerDiamond } from "spinners-react"
 
 class ReservationsList extends Component {
   state = {
     reservations: [], // reservations will ALWAYS be an ARRAY --> The perfect initial state is EMPTY ARRAY
+    isLoading: true,
+    errMessage: "",
   }
 
   componentDidMount() {
@@ -15,16 +18,34 @@ class ReservationsList extends Component {
 
   fetchReservations = async () => {
     // 3.
-    const url = "https://striveschool-api.herokuapp.com/api/reservation"
+    try {
+      const url = "https://striveschool-api.herokuapp.com/api/reservation"
 
-    const response = await fetch(url)
-    if (response.ok) {
-      // server answered with 200 ok :)
-      const data = await response.json()
+      // throw new Error("KABOOOOOOOOOOOOOOOOOOOOOOOOOM!")
+
+      const response = await fetch(url)
+      if (response.ok) {
+        // server answered with 200 ok :)
+        const data = await response.json()
+        this.setState({
+          reservations: data,
+          isLoading: false,
+        })
+      } else {
+        const message = await response.text()
+        this.setState({
+          ...this.state,
+          isLoading: false,
+          errMessage: message,
+        })
+      }
+    } catch (error) {
+      console.log(error)
       this.setState({
-        reservations: data,
+        ...this.state,
+        isLoading: false,
+        errMessage: "Generic Error Happened!",
       })
-    } else {
     }
   }
 
@@ -43,6 +64,18 @@ class ReservationsList extends Component {
       <div>
         <h2>Reservations List!!</h2>
         <ListGroup>
+          {this.state.errMessage && (
+            <Alert variant="danger">{this.state.errMessage}</Alert>
+          )}
+          {this.state.isLoading && (
+            <SpinnerDiamond
+              size={50}
+              thickness={100}
+              speed={100}
+              color="rgba(57, 84, 172, 1)"
+              secondaryColor="rgba(0, 0, 0, 0.44)"
+            />
+          )}
           {this.state.reservations.map((bookedTable, index) => (
             <ListGroup.Item key={index}>
               {bookedTable.name} for {bookedTable.numberOfPeople} at{" "}
